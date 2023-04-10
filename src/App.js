d9383c396b22701a75b2cfff40f27000ecba27d1
serkanguentin <guntinfurkan@gmail.com>
2023-04-10 19:06:49 +0200

    ReactPrjct

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Categories from "./Categories";
+import Navi from "./Navi";
+import Products from "./Products";
+export default class App extends Component {
+  state = { currentCategory: "", products: [] };
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+  };
+  getProducts = () => {
+    fetch("http://localhost:3000/products")
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data }));
+  };
+  render() {
+    let ProductInfo = { title: "Product List" };
+    let CategoryInfo = { title: "Category List" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="4">
+              <Categories
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={ProductInfo}
+              />
+            </Col>
+            <Col xs="8">
+              <Products
+                product={this.state.products}
+                currentCategory={this.state.currentCategory}
+                info={CategoryInfo}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/Products.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export default class Products extends Component {
+  render() {
+    return (
+      <div>
+        <h3>
+          {this.props.info.title}-{this.props.currentCategory}
+        </h3>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.products.map(product => (
+              <tr key={product.id}>
+                <th scope="row">{product.id}</th>
+                <td>{product.productName}</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
